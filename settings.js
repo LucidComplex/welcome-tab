04cd47eb087b0205f2f79f87a0adb07b94e26c62
Eli <eliedrian1996@gmail.com>
2019-04-09 17:10:08 +0800

    settings: Implement close

--- settings.js
+++ settings.js
@@ -5,14 +5,23 @@ const Settings = function(settingsElement, settingsContainerElement) {
 
 	this.init = function() {
 		self.settingsElement.addEventListener('click', self.onClick);
+		const closeElements = self.settingsContainerElement.getElementsByClassName('close');
+		closeElements.item(0).addEventListener('click', self.onCloseClick);
 	};
 
 	this.onClick = function() {
 		self.settingsContainerElement.classList.add('slide-in');
 	};
 
+	this.onCloseClick = function() {
+		self.settingsContainerElement.classList.remove('slide-in');
+	}
+
 	this.setOnClick = function(callback) {
 		self.onClick = callback;
+	};
+
+	this.onClose = function() {
 	};
 
 	this.init();
